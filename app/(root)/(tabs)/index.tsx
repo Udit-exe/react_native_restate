6bc5d534d401d15dcb603b49c6af4d1dc8e8bac8
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2025-01-26 11:50:43 +0000

    home

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,21 @@
-import { Text, View } from "react-native";
+import { Image, SafeAreaView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import images from "@/constants/images";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to reState</Text>
-        
-    </View>
-  );
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row justify-between items-center mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full"/>
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text >Good Morning</Text>
+              <Text>Mojo</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
 }
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,8 +1,11 @@
-import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType, Alert, Button } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import icons from '@/constants/icons';
 import images from '@/constants/images';
+import { settings } from '@/constants/data';
+import { useGlobalContext } from '@/lib/global-provider';
+import { avatar, logout } from '@/lib/appwrite';
 
 interface SettingsItemProps {
   icon: ImageSourcePropType;
@@ -23,7 +26,18 @@ const SettingsItem = ({icon, title, onPress, textStyle, showArrow = true} : Sett
 )
 
 const Profile = () => {
-  const handleLogout = async() => {};
+  
+  const {user, refetch} = useGlobalContext();
+  
+  const handleLogout = async() => {
+    const result = await logout();
+    if(result){
+      Alert.alert("Success" , "You have logged out successfully!" )
+      refetch();
+    }else{
+      Alert.alert("Error", "An error occurred while logging out")
+    }
+  };
 
   return (
     <SafeAreaView className='h-full bg-white'>
@@ -38,11 +52,11 @@ const Profile = () => {
 
         <View className="flex-row flex justify-center mt-5">
           <View className='flex flex-col items-center realtive mt-5'>
-            <Image source={images.avatar} className='size-44 realtive rounded-full'/>
+            <Image source={{uri: user?.avatar}} className='size-44 realtive rounded-full'/>
             <TouchableOpacity className='absolute bottom-11 right-2'>
               <Image source={icons.edit} className='size-9'/>
             </TouchableOpacity>
-            <Text className='text-2xl font-rubik-bold mt-2'> Mojo | Mojo Don</Text>
+            <Text className='text-2xl font-rubik-bold mt-2'>{user?.name}</Text>
           </View>
         </View>
 
@@ -51,8 +65,15 @@ const Profile = () => {
           <SettingsItem icon={icons.wallet} title='Payments'/>
 
         </View>
-        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'>``
-          )}
+        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'>
+          {settings.slice(2).map((item, index) => (
+            <SettingsItem key={index} { ... item }/>
+          ))}
+        </View>
+
+        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'>
+          <SettingsItem icon={icons.logout} title='Logout' textStyle='text-danger' showArrow={false} onPress={handleLogout}/>
+
         </View>
       </ScrollView>
     </SafeAreaView>
