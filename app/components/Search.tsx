36a7fb23cbc0733b1c85209886e6f6c577a12492
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2025-01-27 05:00:04 +0000

    errors fixing

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
 import React, { useState } from 'react'
 import { useLocalSearchParams, usePathname } from 'expo-router'
+import icons from '@/constants/icons';
 
 const Search = () => {
     const path = usePathname();
@@ -12,8 +13,19 @@ const Search = () => {
     }
 
     return (
-    <View className='flex flex-row items-center justify-between w-full'>
-      <Text>Search</Text>
+    <View className='flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2'>
+      <View className='flex-1 flex flex-row items-center justify-start z-50'>
+        <Image source={icons.search} className='size-5 '/>
+        <TextInput
+          value={search}
+          onChangeText = {handleSearch}
+          placeholder='Search for anything'
+          className='text-sm font-rubik text-black-300 ml-2 flex-1'
+        />
+      </View>
+      <TouchableOpacity>
+        <Image source={icons.filter} className='size-5'/>
+      </TouchableOpacity>
     </View>
   )
 }
